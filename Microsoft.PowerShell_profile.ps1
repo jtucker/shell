4323b294e5abb4304e92fa0471eebd9cc60439d3
Jason Tucker <jtucker@gmail.com>
2015-06-17 18:04:47 -0400

    Adding PowerShell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+Function Touch-File
+{
+    $file = $args[0]
+    if($file -eq $null) {
+        throw "No filename supplied"
+    }
+
+    if(Test-Path $file)
+    {
+        (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else
+    {
+        echo $null > $file
+    }
+}
+
+# Aliases
+New-Alias which get-command
+
+# Load posh-git example profile
+. '~\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+
+Import-Module PSGet
+Import-Module PSReadLine
+
+# Load the PowerShell Snap-In (if not available, then just move on.)
+Add-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction Continue
